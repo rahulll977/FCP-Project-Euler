09a91073cf133bccede0ed62762c29fc26e46790
Rahul <65645683+rahulll977@users.noreply.github.com>
2022-04-21 16:40:39 +0530

    Update Even Fibonacci numbers.py

--- Even Fibonacci numbers.py
+++ Even Fibonacci numbers.py
@@ -1,4 +1,6 @@
 # 2.Even Fibonacci numbers
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 n = 4000000
 count = 0
 x = 1  
