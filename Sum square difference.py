434c4742534e0aa12bc73ab19deae84f4b7b948c
Rahul <65645683+rahulll977@users.noreply.github.com>
2022-04-21 16:51:41 +0530

    Update Sum square difference.py

--- Sum square difference.py
+++ Sum square difference.py
@@ -1,12 +1,17 @@
 #6 Sum square difference.
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 N = 100
 count_1 = 0
 count_2 = 0
+
 l1 = [i for i in range(1, N + 1)]
 l2 = [i**2 for i in range(1, N + 1)]
+
 for i in l1:
     count_1 += i
+    
 for i in l2:
     count_2 += i
-print(count_1**2-count_2)
+    
+print(count_1**2 - count_2)
