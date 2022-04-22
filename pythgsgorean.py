c97e1de1b2b9f500a4116aae9fc802bdc6918d5d
Rahul <65645683+rahulll977@users.noreply.github.com>
2022-04-22 11:29:49 +0530

    Add files via upload

---
+++ pythgsgorean.py
@@ -0,0 +1,21 @@
+#9.Special Pythagorean Triplet
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+
+def func():
+	peri = 1000
+	for a in range(1, peri + 1):
+		for b in range(a + 1, peri + 1):
+			c = peri - a - b
+			if a * a + b * b == c * c:
+				return str(a * b * c)
+
+
+if __name__ == "__main__":
+	print(func())
